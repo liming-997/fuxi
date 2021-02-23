497f92d15be2fd32c547c9807c0b7d756635f584
liming <1443516540@qq.com>
2021-02-23 13:20:56 +0800

    双端检索

--- spinLock/src/singleton/Singleton2.java
+++ spinLock/src/singleton/Singleton2.java
@@ -1,15 +1,26 @@
 package singleton;
 
 public class Singleton2 {
+    /**
+     * 构造器私有
+     */
     private Singleton2() {
 
     }
 
     private static Singleton2 singleton2 = null;
 
-    public synchronized static Singleton2 newInStance() {
+    /**
+     * 双端检索 提高效率
+     * @return
+     */
+    public static Singleton2 newInStance() {
         if (singleton2 == null) {
-            singleton2 = new Singleton2();
+            synchronized (Singleton2.class) {
+                if (singleton2 == null) {
+                    singleton2 = new Singleton2();
+                }
+            }
         }
         return singleton2;
     }
